7ce90088fbe1303080e35d3b358f000e856ab044
Nikita Jha <jhanikita85@gmail.com>
2024-06-21 00:49:27 +0530

    Refactor the spec file

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,9 +2,11 @@ require './lib/string_calculator.rb'
 
 describe StringCalculator do 
 
-    #Case 1 : Handle Empty String Input by returning 0 as Input
-    context "Given Empty String" do 
-        it "returns output as 0" do
+    
+    context "Given a String with numbers and add those numbers" do 
+
+        #Case 1 : Handle Empty String Input by returning 0 as Output
+        it "can return output as 0 when string is empty" do
             calc = StringCalculator.new
             sum = calc.add("")
             expect(sum).to eql(0)
