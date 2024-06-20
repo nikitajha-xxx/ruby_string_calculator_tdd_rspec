0b94100e797c8fb2304f657f25b8c70233033e3b
Nikita Jha <jhanikita85@gmail.com>
2024-06-21 01:55:15 +0530

    Pass the Third Test by handling String with two numbers seperated by commas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,9 @@ class StringCalculator
     def add(string_numbers)
         if(string_numbers.empty?)
             return 0
+        elsif(string_numbers.include?(","))
+            numbers = string_numbers.split(",")
+            return (numbers[0].to_i + numbers[1].to_i)
         else
             return string_numbers.to_i
         end
