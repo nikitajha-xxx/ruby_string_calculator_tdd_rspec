b6a718d4358c124d7726ae440caea67f0708a22f
nikitajha-xxx <jhanikita85@gmail.com>
2024-06-21 01:45:42 +0530

    Revert "Pass the Third Test by handling Strings with commas"
    This reverts commit 35fd62b6e2760d09d0c309303f898f01c5cc6232.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,8 +2,6 @@ class StringCalculator
     def add(string_numbers)
         if(string_numbers.empty?)
             return 0
-        elsif(string_numbers.include?(","))
-            return string_numbers.split(",").map(&:to_i).reduce(:+)
         else
             return string_numbers.to_i
         end
