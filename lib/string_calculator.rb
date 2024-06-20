35fd62b6e2760d09d0c309303f898f01c5cc6232
Nikita Jha <jhanikita85@gmail.com>
2024-06-21 01:26:08 +0530

    Pass the Third Test by handling Strings with commas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
     def add(string_numbers)
         if(string_numbers.empty?)
             return 0
+        elsif(string_numbers.include?(","))
+            return string_numbers.split(",").map(&:to_i).reduce(:+)
         else
             return string_numbers.to_i
         end
