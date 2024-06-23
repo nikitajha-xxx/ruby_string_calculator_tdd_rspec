225cf68a3e0ed7b82f506f2ec174e53014f1a6bd
Nikita Jha <jhanikita85@gmail.com>
2024-06-23 18:25:47 +0530

    Refactor Split function

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,12 +9,24 @@ class StringCalculator
     end
 
     def split_numbers(numbers)
-        if(numbers.start_with?("//"))
-            matched_str = numbers.match(/\/\/(.)\n(.*)/)
-            delimiter = matched_str[1]
-            new_num = matched_str[2]
-            return new_num.split(delimiter)
+        if(isCustomDelimiterPresent(numbers))
+            return split_numbers_with_custom_delimiters(numbers)
         end
+        return split_numbers_with_comma_or_new_line(numbers)
+    end
+
+    def isCustomDelimiterPresent(numbers)
+        return numbers.start_with?("//")
+    end
+
+    def split_numbers_with_custom_delimiters(numbers)
+        matched_str = numbers.match(/\/\/(.)\n(.*)/)
+        delimiter = matched_str[1]
+        new_num = matched_str[2]
+        return new_num.split(delimiter)
+    end
+
+    def split_numbers_with_comma_or_new_line(numbers)
         return numbers.split(/[,,\n]/)
     end
 end
